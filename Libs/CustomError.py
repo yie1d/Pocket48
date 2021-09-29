0b43e40afe293a4f41241fd981be848dcaf90351
hexiao <2608019886@qq.com>
2021-09-29 09:42:55 +0800

    update

--- Libs/CustomError.py
+++ Libs/CustomError.py
@@ -1,12 +1,7 @@
-# -*- codeing = utf-8 -*-
-# @Time : 2021/7/29 14:03
-# @Author : XyD3°
-
 class RoomNotFound(Exception):
     '''
     房间不存在
     '''
-
     def __str__(self):
         '''
         错误信息
@@ -20,10 +15,21 @@ class UnknownError(Exception):
     '''
     未知错误类型
     '''
-
     def __str__(self):
         '''
 
         :return:
         '''
         return '未知错误'
+
+
+class NetError(Exception):
+    '''
+    网络错误
+    '''
+    def __str__(self):
+        '''
+
+        :return:
+        '''
+        return '网络错误'
